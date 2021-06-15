64c5cf254da71b16ca617e5f4ebb619a108ad062
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-15 12:47:41 +0500

    Add files via upload

---
+++ butun.py
@@ -0,0 +1,5 @@
+# a va b berilgan a/b bulgandagi butun qismini topilsin butun qismini topsish da // ikkita bulish belgisidan foydalanamiz
+a = float(input("a soni kiritin:"))
+b = float(input("b sonini kiritin:"))
+sum = a // b
+print("butun qismi:",sum)
