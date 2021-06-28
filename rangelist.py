0d79ec34f205947026082d1d5ebe421251dc4d5f
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:52:13 +0500

    Create rangelist.py

---
+++ rangelist.py
@@ -0,0 +1,11 @@
+
+sonlar = list(range(120,1201,2))
+
+print(sonlar)
+print(sum(sonlar)) # sum() sonlar yigindisi topoish uchun foydalanailadi listdagi
+
+print(max(sonlar)-min(sonlar)) # eng katta va eng kichik sonlar ayirmasi topish formulari
+print(len(sonlar))
+print(sonlar[:20])
+
+print(sonlar[-20:]) # bu esa
