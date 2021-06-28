18cd0d227be6c287a9e309b9acc27aa341b9e00a
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:56:41 +0500

    Create taomlist.py

---
+++ taomlist.py
@@ -0,0 +1,19 @@
+
+taomlar = ['osh','manti','shashlik','somsa','jarkob','pelmen','shurva','kucha','']
+taomlar.append('chuchvara')
+taomlar.append('mastava')
+taomlar.append('moshhurda')
+taomlar.append('dimlama')
+taomlar.append('dulma')
+print(taomlar)
+nonushta =taomlar[:]
+nonushta.append('choy')
+nonushta.append('non')
+
+nonushta.remove('osh')
+nonushta.remove('manti')
+nonushta.remove('jarkob')
+nonushta.remove('pelmen')
+nonushta.remove('shurva')
+nonushta.remove('kucha')
+print(nonushta)
