8ae7666d3fab5dc37bba3059ff88cf804f657e4c
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 21:30:03 +0500

    Create looptaklif.py

---
+++ looptaklif.py
@@ -0,0 +1,5 @@
+mehmonlar = ['Ali','Vali','Hasan','Husan','Obid','Sobid']
+for mehmon in mehmonlar:
+    print(f"Hurmatli {mehmon} sizni soat 7 ga iftorlikka lutfan taklif qilamiz")
+print('Hurmat bilan Ergashev lar Oilasi')
+print('Allbatta keling!')
