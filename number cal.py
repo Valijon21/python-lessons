67315b5c8a9e19468fa49f5480bcdb09f78b5428
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:32:33 +0500

    numb

---
+++ number cal.py
@@ -0,0 +1,9 @@
+bir_son = float(input('birinchi sonni kiriting:'))
+ikki_son = float(input("ikkinchi sonni kiriting:"))
+
+qush = bir_son+ikki_son
+ayir = bir_son-ikki_son
+kup = bir_son*ikki_son
+bul = bir_son/ikki_son
+print(f" {bir_son} + {ikki_son} = {qush} \n {bir_son}-{ikki_son}= {ayir}"
+      f"\n {bir_son}*{ikki_son} = {kup} \n {bir_son}/{ikki_son} {bul}")
