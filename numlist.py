29c708868ef21f6701879db80ae37cc335156a4f
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:38:41 +0500

    list2

---
+++ numlist.py
@@ -0,0 +1,16 @@
+sonlar = [17,7,21,1993,-2,56.2,22.3,2]
+print(sonlar)
+qush = sonlar[0]+sonlar[2]
+ayir = sonlar[1]-sonlar[7]
+kupaytir = sonlar[3]*sonlar[4]
+bul = sonlar[4]/sonlar[5]
+ildiz = sonlar[1]**(1/2)
+butun = sonlar[0]%sonlar[7]
+qoldiq = sonlar[1]//sonlar[7]
+kvadrat = sonlar[1]**2
+print(f" yig'indi: {qush} \n ayirma: {ayir} \n kupaytma: {kupaytir}\n bulish:{bul}\n ildiz:{ildiz} \n butun qism:{butun} \n qoldiq{qoldiq}\n kvadrat:{kvadrat}")
+
+sonlar[4] = 2000
+sonlar[5] = 100
+sonlar[6] = sonlar[6]+12.7
+print(sonlar)
