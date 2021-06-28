8b9b75ca6e1c40ac03b7641ebcf1c7d5f1c2921f
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:58:07 +0500

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,7 @@
+tomonlar = (20, 30, 55.2)
+print(tomonlar)
+#  @@@@@@  Tuple ichidagi elementlarga huddi ro'yxat elementlariga murojat qilingani kabi murojat qilinaveradi: ekranga chiqishda
+toys = ('bus','car','bear','dino','snake','lizard')
+print(toys[0])
+print(toys[-1])
+print(toys[2:5])
