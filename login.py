e74daa21eee58339ceffd69e60972a0457cec25a
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 22:20:24 +0500

    log

---
+++ login.py
@@ -0,0 +1,5 @@
+login = input('Yangi login tanlang:')
+if len(login) <= 5: # agar login 5 harifdan kichik bulsa 1
+    print('Login 5 harfdan kup bulishi kerak')
+else:
+    print('login qabul qilindi')
