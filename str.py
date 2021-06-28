b1750fa224d1680db44f30a8e9b5e2a38d96075a
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:19:53 +0500

    str
    string

---
+++ str.py
@@ -0,0 +1,16 @@
+kocha ="bog'bon"
+mahalla ="sog'bon"
+tuman = "Bodomzor"
+viloyat ="Namangan"
+print(kocha + ' ko\'chasi',mahalla+" mahalla",tuman+" Tuman",viloyat+" viloyat")
+kocha = input("kochangizni kiritin:")
+mahalla = input("mahalla kiritin:")
+tuman = input("tuman kiritin:")
+viloyat = input("viloyat kiritin:")
+print(f"{kocha} ko'chasi {mahalla} mahalla {tuman} tumani {viloyat} viloya")
+manzil = f"{kocha} ko'chasi {mahalla} mahallasi {tuman} Tumaningiz {viloyat} Viloyati "
+print(manzil)
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
