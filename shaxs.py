8ad10c5c6f510d8371390d793e2b214d954c4716
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:42:33 +0500

    shaxslar

---
+++ shaxs.py
@@ -0,0 +1,6 @@
+
+t_shaxs = ['Muhammad SAV','Abubakr','Umar','Ali','Usmon''Al Buxoriy','Al Xorazimiy','Beruniy','' ]
+z_shaxs = ['Geys','Aboror Muxtor','Abdug\'ani domla ','Mishary','Shavkat Miramonovich']
+shaxs1= t_shaxs.pop(0)
+shaxs2 = z_shaxs.pop(2)
+print(f"Men tarixiy shaxslardan {shaxs1} bilan zamonaviy shaxslardan esa {shaxs2} bilan suxbat qilishni istardim")
