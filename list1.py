dc0f23c0314f29f4606c3955d49eb08cb03ddd82
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-28 16:36:43 +0500

    lists

---
+++ list1.py
@@ -0,0 +1,4 @@
+'dustlar = ['Doston','O\'ktam','Dadajon']
+print(dustlar)
+xabar='bugun choyxona bormi?'
+print(f" salom {dustlar[0]}  {xabar} \n Borasanmi {dustlar[1]} bugun?\n Sizchi {dustlar[2]} ")
