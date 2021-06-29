d8314f98d8404db0dea86a5c06e7840826489f85
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2021-06-29 14:42:17 +0500

    Create price.py

---
+++ price.py
@@ -0,0 +1,9 @@
+yosh = int(input('yoshingiz nechida?'))
+if yosh <= 4:
+    print('sizga kirish  bepul')
+elif yosh <=12:
+    print('sizga krish 5000 ')
+elif yosh <= 18:
+    print('sizga kris 7000')
+else :
+    print('sizga kirish 10000')
